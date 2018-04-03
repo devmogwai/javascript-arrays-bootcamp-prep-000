2c3f59d25ab88dc92cce47ccc226f712b05ce4cb
Daniel Doughty <djdoughty@gmail.com>
2018-04-03 18:14:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,13 +7,13 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift(element)
   return array
 }
-// function addElementToEndOfArray (array, element) {
-//   return array.push(element)
-// }
-// function destructivelyAddElementToEndOfArray (array, element) {
-//   array = array.push(element)
-//   return array
-// }
+function addElementToEndOfArray (array, element) {
+  return array.push(element)
+}
+function destructivelyAddElementToEndOfArray (array, element) {
+  array = array.push(element)
+  return array
+}
 // function accessElementInArray (array, index) {
   
 // }
