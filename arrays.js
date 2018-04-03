8aa16e41e5505647567187e4a07dfa379dc14974
Daniel Doughty <djdoughty@gmail.com>
2018-04-03 18:32:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,6 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   return array
 }
 function removeElementFromEndOfArray (array) {
-  return array.slice(0, array.length-1)
+  newArray = array.slice(0, array.length-1)
+  return newArray
 }
