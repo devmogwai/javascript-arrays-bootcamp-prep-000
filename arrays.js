245c193d943f902cb904f92a6b63bb28a0ddc7c6
Daniel Doughty <djdoughty@gmail.com>
2018-04-03 18:26:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,6 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 function removeElementFromBeginningOfArray (array) {
   return array.slice(1)
 }
+function removeElementFromBeginningOfArray (array) {
+  return array.slice(1)
+}
