375c7f825c7a3e88cde45befcb7bc2641e40bbeb
Daniel Doughty <djdoughty@gmail.com>
2018-04-03 18:46:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,7 @@ function addElementToBeginningOfArray (array, element) {
   //newArray = array
   //newArray.unshift(element)
   //return newArray
-  array.unshift(element)
-  return array
+  
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
